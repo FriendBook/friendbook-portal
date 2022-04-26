91554e36499c454efe212c14e13b5d34090bebda
jnavacar <jnavacar1998@gmail.com>
2022-04-26 12:12:41 +0200

    Fixed warnings

--- src/Friends/Friends.js
+++ src/Friends/Friends.js
@@ -3,19 +3,19 @@ import axios from "axios";
 
 const Friends = () => {
   const [friends, setFriends] = useState({});
-  const [tempUser, setTempUser] = useState(1);
-
-  const fetchFriends = async () => {
-    axios.get("http://localhost:4000/usr/frnd/" + tempUser);
-    const res = await axios.get("http://localhost:8082/friends/" + tempUser);
-
-    console.log(res);
-    setFriends(res.data);
-  };
-
+  const [tempUser] = useState(1);
+  
   useEffect(() => {
+    async function fetchFriends() {
+      axios.get("http://localhost:4000/usr/frnd/" + tempUser);
+      const res = await axios.get("http://localhost:8082/friends/" + tempUser);
+  
+      setFriends(res.data);
+    }
+
     fetchFriends();
-  }, []);
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
 
   const renderFriends = Object.values(friends).map((friend) => {
     return (
