e7e470a678c8ddce607211054880130dcf94a9f0
jnavacar <jnavacar1998@gmail.com>
2022-05-10 14:30:58 +0200

    testing CD

--- src/Posts/PostPage.js
+++ src/Posts/PostPage.js
@@ -5,7 +5,7 @@ import PostList from "./PostList";
 const PostPage = ({ postCounter, setPostCounter }) => {
   return (
     <div>
-      <h1>Create Post</h1>
+      <h1>Create Post TEST ON CD</h1>
       <PostCreate postCounter={postCounter} setPostCounter={setPostCounter} />
       <hr />
       <h1>Posts</h1>
